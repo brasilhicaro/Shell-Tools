b1fffe8ef5844b99951281a0f726cba1d3304cef
Hicaro_Brasil <hicaro.brasil@hotmail.com>
2024-03-20 14:34:33 -0300

    :bug: fix: conditions

--- salutation.sh
+++ salutation.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-function_salutation(){
+fun_salutation(){
     user_name=$USER
     weekend_day=$(date +%A)
     hours=$(date +%H)
     salutation_text=""
-    if [ $hours >= 5 ] && [ $hours < 12 ]
-        then
-            salutation_text="one good morning"            
-    elif [ $hours >= 12 ] && [ $hours < 18 ]
-        then
-            salutation_text="one good afternoon"
-    elif [ $hours >= 18 ] || [ $hours < 5 ]
-        then
+    if [ "$hours" -ge 5 ] && [ "$hours" -lt 12 ]; then
+        salutation_text="one good morning"            
+    elif [ "$hours" -ge 12 ] && [ "$hours" -lt 18 ]; then
+        salutation_text="one good afternoon"
+    elif [ "$hours" -ge 18 ] || [ "$hours" -lt 5 ]; then
         salutation_text="one good early morning"
     else 
         salutation_text="one good night"
     fi
     echo "Hello $user_name, $salutation_text. Today is $weekend_day."
 }
+
+fun_salutation
